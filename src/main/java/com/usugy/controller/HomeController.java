a69b26abaf5b9421ea37e6a04ce420e8ca1b9cb6
Tomasz Barwicki <tomasz.barwicki@gmail.com>
2016-11-12 21:04:49 +0100

    views

--- src/main/java/com/usugy/controller/HomeController.java
+++ src/main/java/com/usugy/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
 import com.usugy.model.Account;
 import com.usugy.service.AccountService;
 import com.usugy.service.MailService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -23,7 +24,7 @@ import java.security.Principal;
 //@SessionAttributes("account")
 public class HomeController {
 
-
+    private static final Logger logger = Logger.getLogger(HomeController.class);
 
     @Autowired
     private AccountService accountService;
@@ -37,7 +38,7 @@ public class HomeController {
 //        if (principal == null) return "redirect:/signup";
 //        Account account = new Account();
 //        model.addAttribute("account", account);
-
+        logger.info("Entered HomeController");
         return "index3";
     }
 
---
+++ src/test/java/AccountServiceTest.java
@@ -0,0 +1,60 @@
+import com.usugy.model.Account;
+import com.usugy.service.AccountService;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by tomek on 2016-11-11.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/jpaContext.xml",
+        "classpath:/mail-config.xml"})
+
+public class AccountServiceTest {
+
+    private Account account;
+
+    @Autowired
+    private AccountService accountService;
+
+    @BeforeClass
+    public static void before() {
+        System.out.println("BeforeClass");
+    }
+
+    @AfterClass
+    public static void after() {
+        System.out.println("AfterClass");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("Before");
+        account = new Account();
+        account.setFirstname("Jan");
+        account.setLastname("Kowalski");
+        account.setPassword("password");
+        account.setEmail("jan.kowalski@mail.com");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("After");
+    }
+
+    @Test
+    public void createAndSaveNewAccountTest(){
+//        dummyClass.setB(input);
+//        assertEquals(expected, dummyClass.getB());
+        accountService.createAndSaveNewAccount(account);
+        Account tmpAccount = accountService.findByEmail(account.getEmail());
+        assertEquals(account.getEmail(), tmpAccount.getEmail());
+
+    }
+}
